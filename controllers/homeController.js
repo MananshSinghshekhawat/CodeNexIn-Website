ed0762beda108fe312dd045c0dc2dfcb7901b5db
Arjun A <arjun.ashokbgl@gmail.com>
2025-09-07 16:20:45 +0530

    Update homeController.js

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -16,8 +16,8 @@ exports.getHomeContent = async (req, res) => {
     const latestBlogs = await Blog.find({ isPublished: true })
       .sort({ publishedAt: -1 })
       .limit(3)
-      .select('title excerpt slug featuredImage readTime publishedAt author');
-
+      .select('title excerpt slug featuredImage readTime publishedAt')
+      .populate('author', 'name');
 
     const clients = await Client.find({ isActive: true, isFeatured: true })
       .sort({ order: 1 })
