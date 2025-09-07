8f14f3c6c3fddf66e76ce2cca8c9723147768908
Arjun A <arjun.ashokbgl@gmail.com>
2025-09-07 08:15:33 +0530

    Update Service.js

--- models/Service.js
+++ models/Service.js
@@ -4,8 +4,9 @@ const serviceSchema = new mongoose.Schema({
   title: { type: String, required: true },
   description: { type: String, required: true },
   icon: { type: String },
-  features: [Strings]
+  features: [String]
 });
 
 module.exports = mongoose.model("Service", serviceSchema);
+
 
